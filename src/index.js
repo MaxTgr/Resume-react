3ea400674f33451d387d554d90cd987830221325
Max Sylver <raranmariano@hotmail.com>
2018-09-22 17:34:08 -0300

    finished basic site design

--- src/index.js
+++ src/index.js
@@ -2,6 +2,91 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import * as data from './data.json';
+
+// creates the main link buttons on the first page
+function SocialButtons(){
+
+  let ret = [];
+
+  // gets the data from the json and sends to react
+  data.socialButtonsData.forEach((e, index) => {
+      ret.push(<button key={index} className={e.style}><a href={e.link}><i className={e.icon}></i></a></button>);
+  });
+
+  return ret;
+  
+}
+
+function Skills(){
+
+  let ret = [];
+
+  data.skillsData.forEach(e => {
+    ret.push(
+      <div>
+        <span className="tag is-info">{e.area}</span>
+        <p>{e.skillList}</p>
+      </div>
+    );
+  });
+
+  return ret;
+
+}
+
+class ProjectCards extends Component{
+
+  renderIcons(index){
+    let icons = [];
+
+    data.projectCardsData[index].icons.forEach((e, insideIndex) => {
+      icons.push(<i key={insideIndex} className={e}></i>);
+    });
+
+    return icons;
+  }
+
+  renderButtons(index){
+    let buttons = [];
+
+    data.projectCardsData[index].links.forEach((e, insideIndex) => {
+      buttons.push(<a key={insideIndex} href={e.link} className="card-footer-item">{e.name}</a>);
+    });
+    
+    return buttons;
+  }
+
+  render(){
+    let ret = [];
+
+    data.projectCardsData.forEach((e, index) => {
+      ret.push(
+      <div key={index} className="column is-one-fifth">
+        <div className="card has-text-centered">
+          <header className="card-header">
+            <p className="card-header-title">
+              {e.title}
+            </p>
+          </header>
+          <div className="card-content">
+            <div className="content">
+              {e.content}
+              <br/><br/>
+              <h1>{this.renderIcons(index)}</h1>
+            </div>
+          </div>
+          <footer className="card-footer">
+            {this.renderButtons(index)}
+          </footer>
+        </div>
+      </div>)
+    });
+
+    return ret;
+  }
+
+}
 
 class App extends Component {
   openBurger(){
@@ -57,26 +142,59 @@ class App extends Component {
                 Desenvolvedor fullstack e mobile.
               </p>
               <div id="social-links" className="is-size-3-touch is-size-1-desktop">
-                <button class="gh raise"><i className="fab fa-github"></i></button>
-                <button class="in raise"><i className="fab fa-linkedin"></i></button>
-                <button class="email raise"><i className="fas fa-envelope"></i></button>
-                <button class="wp raise"><i className="fab fa-whatsapp"></i></button>
+                <SocialButtons />
               </div>
             </div>
           </div>
         </section>
-        <section className="hero is-info is-fullheight">
-          <div className="hero-body">
-            <div className="container has-text-centered">
-              <h1 className="title is-1">
-                Title 1
-              </h1>
-              <p className="subtitle is-3">
-                Subtitle
-              </p>
+        <section className="hero is-info is-large">
+          <div className="hero-body columns">
+            <div className="is-one-third column">
+                <article className="message is-link">
+                  <div className="message-body">
+                    <h1 className=" is-link is-size-3-touch is-size-1-desktop">Sobre:</h1>
+                    <p>Programador a mais de 2 anos, uso no meu dia a dia em meus projetos mais recentes: nodejs, apis online e databases, amo desenvolvimento mobile, ja fiz 2 apps um deles na play store com mais de 100 downloads, entre meus projetos recentemente terminados tenho 1 chatbot para telegram e tambem um app desktop em electron.<br/><br/>Atualmente me foco estudando maneiras de testar minhas skills, tenho um app em fase de planejamento e um jogo mobile para ser lançado em dezembro!</p>
+                  </div>
+                </article>
+            </div>
+            <div className="is-one-third column">
+              <article className="message is-link">
+                <div className="message-header">
+                  <p>Skills</p>
+                </div>
+                <div className="message-body">
+                  <Skills />
+                </div>
+              </article>
+            </div>
+            <div className="is-one-third column">
+              <iframe id="spotify-frame" title="spotify" src="https://open.spotify.com/embed/user/thefirstmax/playlist/1bl5ohKjwbTnEp0v1UYM5U" width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
             </div>
           </div>
         </section>
+        <section className="hero is-link is-large">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title is-size-3-touch is-size-1-desktop">
+                Projetos
+              </h1>
+            </div>
+            <br/>
+            <div className="columns">
+              <ProjectCards />
+            </div>
+          </div>
+        </section>
+        <footer class="footer">
+          <div class="content has-text-centered">
+            <p>
+              Made with <i className="fas fa-heart"></i> by <a href="http://github.com/maxtgr">MaxTgr</a> with Bulma and React
+            </p>
+            <a href="https://bulma.io">
+              <img src="https://bulma.io/images/made-with-bulma--white.png" alt="Made with Bulma" width="163" height="31"/>
+            </a>
+          </div>
+        </footer>
       </section>
     );
   }
