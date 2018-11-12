2752814cb3a5213a7fcfe5faf6015f297fc5a286
MaxTgr <raranmariano@hotmail.com>
2018-11-12 20:08:27 -0300

    Updates

---
+++ precache-manifest.440589ec842141d13bccf1565925ebc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c84cefbc40bf52820d8",
+    "url": "maxtgr.github.io/Resume-react/static/js/runtime~main.6c84cefb.js"
+  },
+  {
+    "revision": "f7e157c828d08b6c0743",
+    "url": "maxtgr.github.io/Resume-react/static/js/main.f7e157c8.chunk.js"
+  },
+  {
+    "revision": "b1899819fc2a44fdf521",
+    "url": "maxtgr.github.io/Resume-react/static/js/1.b1899819.chunk.js"
+  },
+  {
+    "revision": "f7e157c828d08b6c0743",
+    "url": "maxtgr.github.io/Resume-react/static/css/main.8ee34c03.chunk.css"
+  },
+  {
+    "revision": "14a54ef96645c09d86ff0299df2d660f",
+    "url": "maxtgr.github.io/Resume-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "maxcostadev.me/precache-manifest.79b23a19060ab20823cfd980fc2f821f.js"
+  "maxtgr.github.io/Resume-react/precache-manifest.440589ec842141d13bccf1565925ebc9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("maxcostadev.me/index.html", {
+workbox.routing.registerNavigationRoute("maxtgr.github.io/Resume-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
