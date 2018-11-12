275b04d5e71be2d2a551899d27ca736db9768cb3
MaxTgr <raranmariano@hotmail.com>
2018-11-12 19:45:11 -0300

    Updates

---
+++ precache-manifest.79b23a19060ab20823cfd980fc2f821f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50df8500278aa9fa0d05",
+    "url": "maxcostadev.me/static/js/runtime~main.50df8500.js"
+  },
+  {
+    "revision": "b26797b65e49f8317af2",
+    "url": "maxcostadev.me/static/js/main.b26797b6.chunk.js"
+  },
+  {
+    "revision": "b1899819fc2a44fdf521",
+    "url": "maxcostadev.me/static/js/1.b1899819.chunk.js"
+  },
+  {
+    "revision": "b26797b65e49f8317af2",
+    "url": "maxcostadev.me/static/css/main.8ee34c03.chunk.css"
+  },
+  {
+    "revision": "c26fc9aeee406c7de5c78d4c3230f2c8",
+    "url": "maxcostadev.me/index.html"
+  }
+];
